6f64225470cd59909e8bb341b988e0d545d8c0cd
evprkr <evanjparker@outlook.com>
2022-04-05 21:44:52 -0500

    Created the Editor class, merged the Cursor and Window classes into the Editor.
    Added a function for cursor movement, it keeps you from leaving the
    boundaries of the buffer.
    
    Also added a "cursor_hint" that keeps track of your col position in a
    line so you can move between lines and maintain the same general col
    position when moving from a long line to a short line and then a long
    line again. This is impossible to explain with words.
    
    Ignore the "test.txt" file I'm using for testing the buffer, it's not
    important and you shouldn't look at it.

--- editor.py
+++ editor.py
@@ -1,3 +1,33 @@
 # editor.py
 
+class Editor:
+    def __init__(self, buffer, width, height):
+        self.buffer = buffer
 
+        self.width = width # Terminal width in cols
+        self.height = height # Terminal height in rows
+
+        self.cursor_x = 0; # Cursor position (col)
+        self.cursor_y = 0; # Cursor position (row)
+        self.cursor_hint = 0; # Maintain cursor_x when changing lines
+
+        self.offset_x = 0; # cursor_x minimum value offset
+        self.offset_y = 0; # cursor_y minimum value offset
+
+    def move_cursor(self, key):
+        if key == 'h': # Left
+            if self.cursor_x > self.offset_x:
+                self.cursor_x -= 1
+                self.cursor_hint = self.cursor_x
+        if key == 'l': # Right
+            if self.cursor_x < len(self.buffer[self.cursor_y]) - 1:
+                self.cursor_x += 1
+                self.cursor_hint = self.cursor_x
+        if key == 'k': # Up
+            if self.cursor_y > self.offset_y:
+                self.cursor_y -= 1
+                self.cursor_x = min(self.cursor_hint, len(self.buffer[self.cursor_y]) - 1)
+        if key == 'j': # Down
+            if self.cursor_y < len(self.buffer) - self.offset_y - 1:
+                self.cursor_y += 1
+                self.cursor_x = min(self.cursor_hint, len(self.buffer[self.cursor_y]) - 1)
--- gem.py
+++ gem.py
@@ -4,6 +4,8 @@ import os, sys
 import argparse
 import curses
 
+from editor import *
+
 def main(stdscr):
     parser = argparse.ArgumentParser()
     parser.add_argument("file")
@@ -12,14 +14,18 @@ def main(stdscr):
     with open(args.file) as f:
         buffer = f.readlines()
 
+    editor = Editor(buffer, curses.COLS - 1, curses.LINES - 1)
+
     while True:
         stdscr.erase()
-        for row, line in enumerate(buffer):
-            stdscr.addstr(row, 0, line)
+        for row, line in enumerate(buffer[:editor.height]):
+            stdscr.addstr(row, 0, line[:editor.width])
+
+        stdscr.move(editor.cursor_y, editor.cursor_x)
 
         key = stdscr.getkey()
-        if key == 'q':
-            sys.exit(0)
+        if key == 'q': sys.exit(0) # Exit
+        elif key in 'hjkl': editor.move_cursor(key) # Cursor movement
 
 if __name__ == "__main__":
     curses.wrapper(main)
